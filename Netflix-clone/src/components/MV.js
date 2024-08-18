873eeef73c1adb4612bbfab32da48d754fe7c569
Phillipe Linhares <linharesphillipe@bitbucket.org>
2024-08-17 21:53:41 -0400

    ajustes50

--- Netflix-clone/src/components/MV.js
+++ Netflix-clone/src/components/MV.js
@@ -1,31 +1,14 @@
 // src/components/MV.js
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './MV.css'; 
 
 const MV = ({ title, items }) => {
-    const listRef = useRef(null);
-
-    const scrollLeft = () => {
-        listRef.current.scrollBy({
-            left: -300, // Ajuste o valor para controlar a distância de rolagem
-            behavior: 'smooth'
-        });
-    };
-
-    const scrollRight = () => {
-        listRef.current.scrollBy({
-            left: 300, // Ajuste o valor para controlar a distância de rolagem
-            behavior: 'smooth'
-        });
-    };
-
     return (
         <div className='MV'>
             <h2>{title}</h2>
             <div className="MV--listarea">
-                <button className="carousel-nav left-nav" onClick={scrollLeft}>&lt;</button>
-                <div className="MV--list" ref={listRef}>
+                <div className="MV--list">
                     {items.length > 0 && items.map((item, key) => (
                         <div key={key} className="MV--item">
                             <Link to={`/book/${item._id}`}>
@@ -34,7 +17,6 @@ const MV = ({ title, items }) => {
                         </div>
                     ))}
                 </div>
-                <button className="carousel-nav right-nav" onClick={scrollRight}>&gt;</button>
             </div>
         </div>
     );
