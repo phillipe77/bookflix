b20afece1d65ec2f9bdc1f100b28c2214068e54a
Phillipe Linhares <linharesphillipe@bitbucket.org>
2024-08-18 11:27:41 -0400

    ajustes57

--- Netflix-clone/src/components/MV.js
+++ Netflix-clone/src/components/MV.js
@@ -1,9 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './MV.css';
 
 const MV = ({ title, items }) => {
     const listRef = useRef(null);
+    const [canScrollLeft, setCanScrollLeft] = useState(false);
+    const [canScrollRight, setCanScrollRight] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const { scrollLeft, scrollWidth, clientWidth } = listRef.current;
+            setCanScrollLeft(scrollLeft > 0);
+            setCanScrollRight(scrollLeft < scrollWidth - clientWidth);
+        };
+
+        handleScroll(); // Para verificar no início
+        listRef.current.addEventListener('scroll', handleScroll);
+
+        return () => {
+            listRef.current.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     const handleLeftClick = () => {
         listRef.current.scrollLeft -= 150;
@@ -16,12 +33,17 @@ const MV = ({ title, items }) => {
     return (
         <div className='MV'>
             <h2>{title}</h2>
-            <div 
-                className="MV--listarea" 
-                ref={listRef} 
-                onMouseEnter={() => listRef.current.parentElement.classList.add('show-arrows')}
-                onMouseLeave={() => listRef.current.parentElement.classList.remove('show-arrows')}
-            >
+            {canScrollLeft && (
+                <div className="MV--left" onClick={handleLeftClick}>
+                    <img src="/left-arrow.svg" alt="Left" />
+                </div>
+            )}
+            {canScrollRight && (
+                <div className="MV--right" onClick={handleRightClick}>
+                    <img src="/right-arrow.svg" alt="Right" />
+                </div>
+            )}
+            <div className="MV--listarea" ref={listRef}>
                 <div className="MV--list">
                     {items.length > 0 && items.map((item, key) => (
                         <div key={key} className="MV--item">
@@ -31,12 +53,6 @@ const MV = ({ title, items }) => {
                         </div>
                     ))}
                 </div>
-            </div>
-            <div className="MV--left" onClick={handleLeftClick}>
-                <img src="/left-arrow.svg" alt="Left" />
-            </div>
-            <div className="MV--right" onClick={handleRightClick}>
-                <img src="/right-arrow.svg" alt="Right" />
             </div>
         </div>
     );
