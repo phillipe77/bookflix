30b2186634d67657aa99ed2b17c27e3c99429a93
Phillipe Linhares <linharesphillipe@bitbucket.org>
2024-08-18 02:35:48 -0400

    ajustes54

--- Netflix-clone/src/components/MV.js
+++ Netflix-clone/src/components/MV.js
@@ -1,13 +1,27 @@
-// src/components/MV.js
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import './MV.css'; 
+import './MV.css';
 
 const MV = ({ title, items }) => {
+    const listRef = useRef(null);
+
+    const handleLeftClick = () => {
+        listRef.current.scrollLeft -= 150;
+    };
+
+    const handleRightClick = () => {
+        listRef.current.scrollLeft += 150;
+    };
+
     return (
         <div className='MV'>
             <h2>{title}</h2>
-            <div className="MV--listarea">
+            <div 
+                className="MV--listarea" 
+                ref={listRef} 
+                onMouseEnter={() => listRef.current.parentElement.classList.add('show-arrows')}
+                onMouseLeave={() => listRef.current.parentElement.classList.remove('show-arrows')}
+            >
                 <div className="MV--list">
                     {items.length > 0 && items.map((item, key) => (
                         <div key={key} className="MV--item">
@@ -17,6 +31,12 @@ const MV = ({ title, items }) => {
                         </div>
                     ))}
                 </div>
+            </div>
+            <div className="MV--left" onClick={handleLeftClick}>
+                <img src="/left-arrow.svg" alt="Left" />
+            </div>
+            <div className="MV--right" onClick={handleRightClick}>
+                <img src="/right-arrow.svg" alt="Right" />
             </div>
         </div>
     );
