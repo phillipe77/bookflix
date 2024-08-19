539746ac7448bf519087ce0482b52e7f43b03085
Phillipe Linhares <linharesphillipe@bitbucket.org>
2024-08-18 23:35:34 -0400

    ajustes94

--- Netflix-clone/src/App.js
+++ Netflix-clone/src/App.js
@@ -2,13 +2,12 @@ import React, { useEffect, useState, useMemo, useCallback, lazy, Suspense } from
 import bookApi from './bookApi';
 import MV from './components/MV';
 import Header from './components/Header';
-import Login from './components/Login';
+import Login from './components/Login'; // Adicionei o componente de Login
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import { Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
-// Lazy loading para os componentes
 const BookDetails = lazy(() => import('./components/BookDetails'));
 const Fm = lazy(() => import('./components/fm'));
 
@@ -16,11 +15,19 @@ const App = () => {
     const [bookList, setBookList] = useState([]); 
     const [featureData, setFeatureData] = useState([]);
     const [blackHeader, setBlackHeader] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticação
 
-    // Verificar se o usuário está autenticado
-    const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+    const handleLogin = (status) => {
+        setIsAuthenticated(status);
+        // Poderia salvar no local storage para persistir o login
+        localStorage.setItem('isAuthenticated', status);
+    };
 
-    // Carrega os dados dos livros uma vez
+    const handleLogout = () => {
+        setIsAuthenticated(false);
+        localStorage.removeItem('isAuthenticated');
+    };
+
     useEffect(() => {
         const loadAll = async () => {
             let list = await bookApi.getHomeList();
@@ -36,14 +43,12 @@ const App = () => {
         loadAll();
     }, []);
 
-    // Memoriza o valor de bookList para evitar re-renderizações desnecessárias
     const memoizedBookList = useMemo(() => {
         return bookList.map((item, key) => (
             <MV key={key} title={item.title} items={item.items} />
         ));
     }, [bookList]);
 
-    // Usa callback memoizada para o listener de scroll
     const handleScroll = useCallback(() => {
         if (window.scrollY > 15) {
             setBlackHeader(true);
@@ -59,55 +64,48 @@ const App = () => {
         };
     }, [handleScroll]);
 
+    useEffect(() => {
+        const authStatus = localStorage.getItem('isAuthenticated') === 'true';
+        setIsAuthenticated(authStatus);
+    }, []);
+
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-            <Router>
+            {isAuthenticated ? (
                 <div className="page">
-                    {isAuthenticated ? (
-                        <>
-                            <Header black={blackHeader} />
-                            <Routes>
-                                <Route path="/" element={
-                                    <Suspense fallback={<div>Loading...</div>}>
-                                        {featureData && <Fm item={featureData} />}
-                                        <section className="lists">
-                                            {memoizedBookList}
-                                        </section>
-                                        <footer>
-                                            Desenvolvido por Phillipe Linhares<br />
-                                            GitHub: https://github.com/phillipe77<br />
-                                            Direito de imagem para Netflix<br />
-                                        </footer>
-                                        {bookList.length <= 0 && (
-                                            <div className="loading">
-                                                <img
-                                                    src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
-                                                    alt="loading"
-                                                />
-                                            </div>
-                                        )}
-                                    </Suspense>
-                                } />
-                                
-                                {/* Rota para a página de detalhes do livro */}
-                                <Route path="/book/:id" element={
-                                    <Suspense fallback={<div>Loading...</div>}>
-                                        <BookDetails />
-                                    </Suspense>
-                                } />
-                                {/* Redireciona qualquer rota desconhecida para a página principal */}
-                                <Route path="*" element={<Navigate to="/" />} />
-                            </Routes>
-                        </>
-                    ) : (
-                        <Routes>
-                            <Route path="/login" element={<Login />} />
-                            {/* Redireciona para a página de login se não estiver autenticado */}
-                            <Route path="*" element={<Navigate to="/login" />} />
-                        </Routes>
-                    )}
+                    <Header black={blackHeader} onLogout={handleLogout} />
+                    <Routes>
+                        <Route path="/" element={
+                            <Suspense fallback={<div>Loading...</div>}>
+                                {featureData && <Fm item={featureData} />}
+                                <section className="lists">
+                                    {memoizedBookList}
+                                </section>
+                                <footer>
+                                    Desenvolvido por Phillipe Linhares<br />
+                                    GitHub: https://github.com/phillipe77<br />
+                                    Direito de imagem para Netflix<br />
+                                </footer>
+                                {bookList.length <= 0 && (
+                                    <div className="loading">
+                                        <img
+                                            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+                                            alt="loading"
+                                        />
+                                    </div>
+                                )}
+                            </Suspense>
+                        } />
+                        <Route path="/book/:id" element={
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <BookDetails />
+                            </Suspense>
+                        } />
+                    </Routes>
                 </div>
-            </Router>
+            ) : (
+                <Login onLogin={handleLogin} />
+            )}
         </Worker>
     );
 };
