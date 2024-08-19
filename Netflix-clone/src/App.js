9d55aafefa3a08ff4e337972eef9205c43870846
Phillipe Linhares <linharesphillipe@bitbucket.org>
2024-08-18 23:57:20 -0400

    ajustes99

--- Netflix-clone/src/App.js
+++ Netflix-clone/src/App.js
@@ -2,48 +2,66 @@ import React, { useEffect, useState, useMemo, useCallback, lazy, Suspense } from
 import bookApi from './bookApi';
 import MV from './components/MV';
 import Header from './components/Header';
+import Login from './components/Login'; // Importando o componente de Login
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
-// Lazy loading para os componentes
 const BookDetails = lazy(() => import('./components/BookDetails'));
 const Fm = lazy(() => import('./components/fm'));
-const Login = lazy(() => import('./components/Login'));
 
 const App = () => {
     const [bookList, setBookList] = useState([]); 
     const [featureData, setFeatureData] = useState([]);
     const [blackHeader, setBlackHeader] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('isAuthenticated') === 'true');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    // Carrega os dados dos livros uma vez
+    const navigate = useNavigate();
+
+    // Verifica se o usuário já está logado ao carregar a página
     useEffect(() => {
-        if (isAuthenticated) {
-            const loadAll = async () => {
-                let list = await bookApi.getHomeList();
-                setBookList(list);
-
-                let allBooks = list[0].items;
-                let randomChosen = Math.floor(Math.random() * (allBooks.length - 1));
-                let chosen = allBooks[randomChosen];
-                let chosenInfo = await bookApi.getBookInfo(chosen._id);
-
-                setFeatureData(chosenInfo);
-            };
-            loadAll();
+        const auth = localStorage.getItem('isAuthenticated');
+        if (auth) {
+            setIsAuthenticated(true);
+        } else {
+            navigate('/login');
         }
-    }, [isAuthenticated]);
+    }, [navigate]);
 
-    // Memoriza o valor de bookList para evitar re-renderizações desnecessárias
+    const handleLogin = () => {
+        localStorage.setItem('isAuthenticated', 'true');
+        setIsAuthenticated(true);
+        navigate('/');
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('isAuthenticated');
+        setIsAuthenticated(false);
+        navigate('/login');
+    };
+
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await bookApi.getHomeList();
+            setBookList(list);
+            
+            let allBooks = list[0].items;
+            let randomChosen = Math.floor(Math.random() * (allBooks.length - 1));
+            let chosen = allBooks[randomChosen];
+            let chosenInfo = await bookApi.getBookInfo(chosen._id);
+
+            setFeatureData(chosenInfo);
+        };
+        loadAll();
+    }, []);
+
     const memoizedBookList = useMemo(() => {
         return bookList.map((item, key) => (
             <MV key={key} title={item.title} items={item.items} />
         ));
     }, [bookList]);
 
-    // Usa callback memoizada para o listener de scroll
     const handleScroll = useCallback(() => {
         if (window.scrollY > 15) {
             setBlackHeader(true);
@@ -59,58 +77,47 @@ const App = () => {
         };
     }, [handleScroll]);
 
-    const handleLogout = () => {
-        localStorage.removeItem('isAuthenticated');
-        setIsAuthenticated(false);
-    };
-
-    if (!isAuthenticated) {
-        return (
-            <Suspense fallback={<div>Loading...</div>}>
-                <Login onLogin={() => setIsAuthenticated(true)} />
-            </Suspense>
-        );
-    }
-
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
             <Router>
                 <div className="page">
-                    <Header black={blackHeader} onLogout={handleLogout} />
-                    <Routes>
-                        {/* Rota para a página inicial */}
-                        <Route path="/" element={
-                            <Suspense fallback={<div>Loading...</div>}>
-                                {featureData && <Fm item={featureData} />}
-                                <section className="lists">
-                                    {memoizedBookList}
-                                </section>
-                                <footer>
-                                    Desenvolvido por Phillipe Linhares<br />
-                                    GitHub: https://github.com/phillipe77<br />
-                                    Direito de imagem para Netflix<br />
-                                </footer>
-                                {bookList.length <= 0 && (
-                                    <div className="loading">
-                                        <img
-                                            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
-                                            alt="loading"
-                                        />
-                                    </div>
-                                )}
-                            </Suspense>
-                        } />
-                        
-                        {/* Rota para a página de detalhes do livro */}
-                        <Route path="/book/:id" element={
-                            <Suspense fallback={<div>Loading...</div>}>
-                                <BookDetails />
-                            </Suspense>
-                        } />
-
-                        {/* Rota para redirecionamento se não estiver autenticado */}
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </Routes>
+                    {isAuthenticated ? (
+                        <>
+                            <Header black={blackHeader} onLogout={handleLogout} />
+                            <Routes>
+                                <Route path="/" element={
+                                    <Suspense fallback={<div>Loading...</div>}>
+                                        {featureData && <Fm item={featureData} />}
+                                        <section className="lists">
+                                            {memoizedBookList}
+                                        </section>
+                                        <footer>
+                                            Desenvolvido por Phillipe Linhares<br />
+                                            GitHub: https://github.com/phillipe77<br />
+                                            Direito de imagem para Netflix<br />
+                                        </footer>
+                                        {bookList.length <= 0 && (
+                                            <div className="loading">
+                                                <img
+                                                    src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+                                                    alt="loading"
+                                                />
+                                            </div>
+                                        )}
+                                    </Suspense>
+                                } />
+                                <Route path="/book/:id" element={
+                                    <Suspense fallback={<div>Loading...</div>}>
+                                        <BookDetails />
+                                    </Suspense>
+                                } />
+                            </Routes>
+                        </>
+                    ) : (
+                        <Routes>
+                            <Route path="/login" element={<Login onLogin={handleLogin} />} />
+                        </Routes>
+                    )}
                 </div>
             </Router>
         </Worker>
