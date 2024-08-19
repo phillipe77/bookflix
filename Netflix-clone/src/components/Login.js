15a367653be29ab909952f0fc459e9224b614e50
Phillipe Linhares <linharesphillipe94@gmail.com>
2024-08-19 14:33:05 -0400

    auth front config3

--- Netflix-clone/src/components/Login.js
+++ Netflix-clone/src/components/Login.js
@@ -10,7 +10,7 @@ const Login = ({ onLogin }) => {
         e.preventDefault();
 
         try {
-            const response = await fetch('/api/login', {
+            const response = await fetch('/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
