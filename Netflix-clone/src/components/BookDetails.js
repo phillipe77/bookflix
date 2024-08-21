d3890962689ce41f1dd1f0dd6ce28197ec00aaec
Phillipe Linhares <linharesphillipe94@gmail.com>
2024-08-21 14:28:27 -0400

    cyntler42

--- Netflix-clone/src/components/BookDetails.js
+++ Netflix-clone/src/components/BookDetails.js
@@ -77,7 +77,7 @@ const BookDetails = () => {
             </div>
             <div className="view-buttons">
                 <button onClick={handleReadNow} className="view-button">LER AGORA</button>
-                <button onClick={handleDownload} className="view-button">FAZER DONWLOAD</button>
+                <button onClick={handleDownload} className="view-button">FAZER DOWNLOAD</button>
             </div>
             <p className="book-note">
                 <strong>OBS:</strong> <strong>SE VOCÊ NOTAR QUE O LIVRO ESTÁ DEMORANDO PARA CARREGAR, PODE SER PORQUE O LIVRO SEJA DO TIPO QUE FOI ESCANEADO E ISSO CAUSA LENTIDÃO. PARA UMA LEITURA MAIS TRANQUILA, EXPERIMENTE FAZER O DONWLOAD</strong>
